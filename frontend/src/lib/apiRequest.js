504b7b8ae58edbc893a7af86ba790d4407b0dfb6
taiga-shiokawa <taiga.hr12@gmail.com>
2024-12-16 20:05:04 +0900

    axios修正

--- frontend/src/lib/apiRequest.js
+++ frontend/src/lib/apiRequest.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 const apiRequest = axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL: import.meta.env.MODE === "development" 
+    ? "http://localhost:3000/api" 
+    : "/api",
   withCredentials: true
 });
 
