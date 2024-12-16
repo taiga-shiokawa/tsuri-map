771591b5d36078d3d0649b412e58440c6b250ae6
taiga-shiokawa <taiga.hr12@gmail.com>
2024-12-16 17:02:01 +0900

    app.js修正

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,8 @@ import cookieParser from "cookie-parser";
 import cors from "cors";
 import dotenv from "dotenv";
 import express from "express";
+import path from 'path';
+import { fileURLToPath } from 'url';
 
 import AuthRouter from "./routes/auth.route.js";
 import UserRouter from "./routes/user.route.js";
@@ -12,10 +14,20 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+
 app.use(cors({origin: process.env.CLIENT_URL, credentials: true}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+
+app.use(express.static(path.join(__dirname, 'frontend/dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'frontend/dist/index.html'));
+});
 
 app.use("/api/auth", AuthRouter);
 app.use("/api/users", UserRouter);
