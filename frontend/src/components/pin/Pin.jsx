b28032bcb784deb353c288bdff175f56778dde8d
taiga-shiokawa <taiga.hr12@gmail.com>
2024-12-16 20:14:13 +0900

    マップ、ピン出現

--- frontend/src/components/pin/Pin.jsx
+++ frontend/src/components/pin/Pin.jsx
@@ -1,6 +1,16 @@
 import { Marker, Popup } from "react-leaflet";
 import WeatherDisplay from "../../utils/WeatherDisplay";
 import FishingTypeDisplay from "../../utils/FishingTypeDisplay";
+import L from 'leaflet';
+import 'leaflet/dist/leaflet.css';
+
+// デフォルトアイコンの設定
+delete L.Icon.Default.prototype._getIconUrl;
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon-2x.png',
+  iconUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon.png',
+  shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png',
+});
 
 const Pin = ({item}) => {
   if (!item || !item.location) return null;
@@ -11,7 +21,7 @@ const Pin = ({item}) => {
     <Marker position={[item.location.latitude, item.location.longitude]}>
       <Popup>
         <div className="p-2">
-        <h3 className="font-bold">
+          <h3 className="font-bold">
             <a 
               href={searchUrl} 
               target="_blank" 
