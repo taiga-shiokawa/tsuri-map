2b541652a76daf64c9fbafd58bfe86b74be10ff8
taiga-shiokawa <taiga.hr12@gmail.com>
2024-12-16 17:56:48 +0900

    bcryptjsに変更

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,4 +1,4 @@
-import bcrypt from "bcrypt";
+import bcrypt from "bcryptjs";
 import prisma from "../lib/prisma.js";
 import jwt from "jsonwebtoken";
 import { sendWelcomeEmail } from "../emails/emailHandlers.js";
